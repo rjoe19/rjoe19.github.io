ff177c6ce30411fd4158e97675b21fe0b2ce5a1a
Richard Joe <rjoewgrr@gmail.com>
2015-12-04 08:16:43 +1300

    Add todolist complete

--- projects/list.js
+++ projects/list.js
@@ -7,25 +7,30 @@ var inputs = todolist.querySelectorAll("input");
 for (var i=0; i<items.length; i++) {
   items[i].addEventListener("click", editItem);
    inputs[i].addEventListener("blur", updateItem);
+   inputs[i].addEventListener("keypress", itemKeypress);
 }
 
 function editItem() {
+  console.log(this);
   this.className = "edit";
   var input = this.querySelector("input");
   input.focus();
   console.log("my current value is", input.value.length);
-  input.setSelectionRange(0, input.value.length); 
+  input.setSelectionRange(0, input.value.length);
 } 
 
 function updateItem() {
+  console.log("we blurred!", this.value);
   this.previousElementSibling.innerHTML = this.value;
   this.parentNode.className = "";
 }
 
-//* figure out what this means
+
 function itemKeypress (event) { 
+  console.log(event);
   if (event.which === 13) {
     updateItem.call(this);
   }
-  *//
+  
 }
+
